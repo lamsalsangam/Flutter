e9035b4e304ffa54a8efb9484e48722370f89107
lamsalsangam <me.sangam.lamsal.10@gmail.com>
2023-04-17 06:40:19 +0545

    Buttons and COntainer

--- learn/lib/main.dart
+++ learn/lib/main.dart
@@ -28,23 +28,72 @@ class Home extends StatelessWidget {
       ),
       // Primary content of the Scaffold displayed below the Scaffold
       // Centering the body content
-      body: Center(
-        // child: Text('Hello, World!',
-        //   // Adding the styles to the text
-        //   style: TextStyle(fontSize: 20.0,
-        //       fontWeight: FontWeight.bold,
-        //       letterSpacing: 2.0,
-        //       color: Colors.grey[600],
-        //       fontFamily: 'GentiumBookPlus'),),
-        //   child: Image(
-        //     // For the network image
-        //     // image: NetworkImage('https://images.unsplash.com/photo-1516331590554-978b8de25f18?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1172&q=80'),
-        //     // For the local image
-        //     image: AssetImage('assets/space-2.jpg'),
-        //   ),
-        // Another way to get image is
-        child: Image.asset('assets/space-1.jpg'),
-        // child: Image.network('https://images.unsplash.com/photo-1516331590554-978b8de25f18?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1172&q=80'),
+      // body: Center(
+      //   // child: Text('Hello, World!',
+      //   //   // Adding the styles to the text
+      //   //   style: TextStyle(fontSize: 20.0,
+      //   //       fontWeight: FontWeight.bold,
+      //   //       letterSpacing: 2.0,
+      //   //       color: Colors.grey[600],
+      //   //       fontFamily: 'GentiumBookPlus'),),
+      //   //   child: Image(
+      //   //     // For the network image
+      //   //     // image: NetworkImage('https://images.unsplash.com/photo-1516331590554-978b8de25f18?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1172&q=80'),
+      //   //     // For the local image
+      //   //     image: AssetImage('assets/space-2.jpg'),
+      //   //   ),
+      //   // Another way to get image is
+      //   // child: Image.asset('assets/space-1.jpg'),
+      //   // child: Image.network('https://images.unsplash.com/photo-1516331590554-978b8de25f18?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1172&q=80'),
+      //   // For the icons to be used form the material design library
+      //   // child: Icon(
+      //   //   Icons.airport_shuttle,
+      //   //   color: Colors.lightBlue,
+      //   //   size: 50.0,
+      //   // ),
+      //   // child: ElevatedButton(
+      //   //   onPressed: (){},
+      //   //   child: Text('Elevated Button'),
+      //   //   style: ElevatedButton.styleFrom(
+      //   //     backgroundColor: Colors.lightBlue,
+      //   //   ),
+      //   // ),
+      //   // child: TextButton(
+      //   //   onPressed: (){},
+      //   //   child: Text('Text button'),
+      //   //   style: TextButton.styleFrom(
+      //   //     foregroundColor: Colors.black,
+      //   //     backgroundColor: Colors.lightBlue,
+      //   //   ),
+      //   // ),
+      //   // Button with the icon inside
+      //   // child: ElevatedButton.icon(
+      //   //   onPressed: (){},
+      //   //   icon: const Icon(
+      //   //     Icons.mail,
+      //   //     // color: Colors.black,
+      //   //   ),
+      //   //   label: Text('mail me'),
+      //   //   style: ElevatedButton.styleFrom(
+      //   //     backgroundColor: Colors.amber,
+      //   //     foregroundColor: Colors.black,
+      //   //   ),
+      //   // ),
+      //   // Icon Button
+      //   // child: IconButton(
+      //   //   onPressed: (){},
+      //   //   icon: const Icon(
+      //   //       Icons.mail_outline_sharp
+      //   //   ),
+      //   //    color: Colors.blueGrey,
+      //   // ),
+      body: Container(
+        // padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 10.0),
+        // padding: EdgeInsets.all(20.0),
+        padding: EdgeInsets.fromLTRB(10.0, 20.0, 30.0, 40.0),
+        margin: EdgeInsets.all(30.0),
+        color: Colors.grey.shade400,
+        child: Text('Hello World'),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: startHammering,
