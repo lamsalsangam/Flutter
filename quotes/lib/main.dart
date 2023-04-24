96339fae5ccbd332ca695af549e3c45fe77fc6ac
lamsalsangam <me.sangam.lamsal.10@gmail.com>
2023-04-24 08:12:44 +0545

    Making List of Object

--- quotes/lib/main.dart
+++ quotes/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'quote.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -12,11 +13,11 @@ class QuoteList extends StatefulWidget{
 }
 
 class _QuoteListState extends State<QuoteList>{
-  List<String> quotes = [
-    "Be yourself; Every One else is already taken.",
-    "I have nothing to declare except my genius",
-    "The truth is rarely pure and never simple",
-    "Success is not final; yeah failure can be fatal; but it is the courage to continue that counts.",
+  List<Quote> quotes = [
+    Quote(author: 'Oscar Wilde', text: 'Be yourself; everyone else is already taken'),
+    Quote(author: 'Oscar Wilde', text: 'I have nothing to declare except my genius'),
+    Quote(author: 'Oscar Wilde', text: 'The truth is rarely pure and never simple'),
+    Quote(author: "NoOne", text: "Success is not final; yeah failure can be fatal; but it is the courage to continue that counts.")
   ];
 
   @override
@@ -30,7 +31,7 @@ class _QuoteListState extends State<QuoteList>{
       ),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
-        children: quotes.map((quote) => Text(quote)).toList(),
+        children: quotes.map((quote) => Text("${quote.text} - ${quote.author}")).toList(),
       ),
     );
   }
---
+++ quotes/lib/quote.dart
@@ -0,0 +1,8 @@
+class Quote {
+  late String text;
+  late String author;
+
+  Quote({required this.text, required this.author});
+}
+
+// Quotes myquote = Quotes(text: "If the world did have happiness.", author: "Sangam Lamsal");
