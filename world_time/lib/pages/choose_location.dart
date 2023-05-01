2a26b4db56d1fbd828f156a87a11cace237483c5
lamsalsangam <me.sangam.lamsal.10@gmail.com>
2023-05-01 15:37:24 +0545

    List View Builder

--- world_time/lib/pages/choose_location.dart
+++ world_time/lib/pages/choose_location.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:world_time/services/world_time.dart';
+
 
 class ChooseLocation extends StatefulWidget{
   @override
@@ -7,6 +9,17 @@ class ChooseLocation extends StatefulWidget{
 
 class _ChooseLocationState extends State<ChooseLocation>{
   // int counter = 0;
+  List<WorldTime> locations = [
+    WorldTime(url: 'Europe/London', location: 'London', flag: 'uk.png'),
+    WorldTime(url: 'Europe/Athens', location: 'Athens', flag: 'greece.png'),
+    WorldTime(url: 'Africa/Cairo', location: 'Cairo', flag: 'egypt.png'),
+    WorldTime(url: 'Africa/Nairobi', location: 'Nairobi', flag: 'kenya.png'),
+    WorldTime(url: 'America/Chicago', location: 'Chicago', flag: 'usa.png'),
+    WorldTime(url: 'America/New_York', location: 'New York', flag: 'usa.png'),
+    WorldTime(url: 'Asia/Seoul', location: 'Seoul', flag: 'south_korea.png'),
+    WorldTime(url: 'Asia/Jakarta', location: 'Jakarta', flag: 'indonesia.png'),
+    WorldTime(location: "Kathmandu", flag: "nepal.png", url: "Asia/Kathmandu"),
+  ];
 
   @override
   Widget build(BuildContext context){
@@ -18,6 +31,23 @@ class _ChooseLocationState extends State<ChooseLocation>{
         title: const Text('Choose a Location'),
         centerTitle: true,
         elevation: 0,
+      ),
+      body: ListView.builder(
+        itemCount: locations.length,
+        itemBuilder: (context, index){
+          return Padding(
+            padding: const EdgeInsets.symmetric(vertical: 1.0, horizontal: 4.0),
+            child: Card(
+              child: ListTile(
+                onTap: (){},
+                title: Text(locations[index].location.toString()),
+                leading: CircleAvatar(
+                  backgroundImage:  AssetImage('assets/${locations[index].flag}'),
+                ),
+              ),
+            ),
+          );
+        },
       ),
       // body: ElevatedButton(onPressed: (){
       //   setState(() {
