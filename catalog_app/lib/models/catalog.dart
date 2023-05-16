7990428cb0b7a254fa61af689e776815299a622e
lamsalsangam <me.sangam.lamsal.10@gmail.com>
2023-05-17 05:35:55 +0545

    (Models | AppBarTheme)

---
+++ catalog_app/lib/models/catalog.dart
@@ -0,0 +1,21 @@
+class Item {
+  final String id;
+  final String name;
+  final String desc;
+  final num price;
+  final String color;
+  final String image;
+
+  Item({required this.id, required this.name, required this.desc, required this.price, required this.color, required this.image});
+}
+
+final products = [
+  Item(
+      id: "SangamStore001",
+      name: "iPhone 14 Pro",
+      desc: "Apple iPhone 14th generation",
+      price: 1499,
+      color: "#33505a",
+      image:
+      "https://www.apple.com/v/iphone-14-pro/h/images/key-features/features/dynamic-island/dd_dynamic_island_bubbles__eta5uuag8ay6_medium.jpg")
+];
