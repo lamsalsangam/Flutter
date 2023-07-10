bcee7ad9d45185da1e03fb29c5eb4df4c444ce36
lamsalsangam <me.sangam.lamsal.10@gmail.com>
2023-07-10 13:21:30 +0545

    Starting the new Layout Project

--- mynotes/lib/main.dart
+++ mynotes/lib/main.dart
@@ -1,7 +1,11 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:mynotes/views/login_view.dart';
-import 'package:mynotes/views/register_view.dart';
+import 'package:mynotes/firebase_options.dart';
+// import 'package:mynotes/views/login_view.dart';
+// import 'package:mynotes/views/register_view.dart';
 
 
 Future<void> main() async {
@@ -13,9 +17,40 @@ Future<void> main() async {
       colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       useMaterial3: true,
     ),
-    home: const RegisterView(),
+    home: const HomePage(),
   ));
 }
 
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
 
-
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Home Page"),
+      ),
+      body: FutureBuilder(
+        future: Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform),
+        builder: (context,snapshot){
+          switch (snapshot.connectionState){
+            case ConnectionState.done:
+              final user = FirebaseAuth.instance.currentUser;
+              if (user?.emailVerified ?? false){
+                if (kDebugMode) {
+                  print("You are verified user");
+                }
+              }else{
+                if (kDebugMode) {
+                  print("You are yet to be verified");
+                }
+              }
+              return const Text("Done");
+            default:
+              return const Text("Loading");
+          }
+        },
+      ),
+    );
+  }
+}
