83c6a59df3edd0eeee609f3c7b9f3683068718b5
lamsalsangam <me.sangam.lamsal.10@gmail.com>
2023-08-23 17:27:45 +0545

    Created the chat heads for the user

--- chat_app/lib/main.dart
+++ chat_app/lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: "Chat App",
       theme: ThemeData.dark(
         useMaterial3: true,
--- chat_app/lib/screen/chats_listing.dart
+++ chat_app/lib/screen/chats_listing.dart
@@ -12,7 +12,42 @@ class ChatsListing extends StatelessWidget {
     return Scaffold(
       appBar: buildChatAppBar(context),
       drawer: buildChatDrawer(),
+      body: Padding(
+        padding: const EdgeInsets.only(top: 10,left: 8.0,right: 8.0, bottom: 15),
+        child: Column(
+          children: [
+            SizedBox(
+              height: 106,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: 10,
+                itemBuilder: (context, index) {
+                  return const Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Column(
+                      children: [
+                        CircleAvatar(
+                          radius: 25,
+                          backgroundImage: AssetImage("assets/images/wall.jpg"),
+                        ),
+                        SizedBox(
+                          width: 50,
+                          child: Text(
+                            "@name giver",
+                            maxLines: 2,
+                            overflow: TextOverflow.ellipsis,
+                            textAlign: TextAlign.center,
+                          ),
+                        )
+                      ],
+                    ),
+                  );
+                },
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
-
