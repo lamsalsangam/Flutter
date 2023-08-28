c1209644156ba484a7a08e34cf8b2cdc992a5380
lamsalsangam <me.sangam.lamsal.10@gmail.com>
2023-08-28 06:41:55 +0545

    Made the Chat Page whole layout.

--- chat_app/lib/screen/chat.dart
+++ chat_app/lib/screen/chat.dart
@@ -1,12 +1,38 @@
 import 'package:flutter/material.dart';
-
 import '../components/chat_header.dart';
 import '../components/message_card.dart';
 
-class Chat extends StatelessWidget {
-  const Chat({super.key});
+class Chat extends StatefulWidget {
+  const Chat({Key? key}) : super(key: key);
 
   static const String routeName = "/chat";
+
+  @override
+  State<Chat> createState() => _ChatState();
+}
+
+class _ChatState extends State<Chat> {
+  final TextEditingController _messageController = TextEditingController();
+  final List<Widget> _messages = [];
+
+  void _sendMessage() {
+    String message = _messageController.text;
+    if (message.isNotEmpty) {
+      setState(() {
+        _messages.add(MessageCard(
+          message: message,
+          isSender: true,
+        ));
+      });
+      _messageController.clear();
+      // Scroll to the bottom of the ListView after sending a message
+      Future.delayed(const Duration(milliseconds: 100), () {
+        _scrollController.jumpTo(_scrollController.position.maxScrollExtent);
+      });
+    }
+  }
+
+  final ScrollController _scrollController = ScrollController();
 
   @override
   Widget build(BuildContext context) {
@@ -20,28 +46,42 @@ class Chat extends StatelessWidget {
             Expanded(
               child: Container(
                 color: Colors.black54,
-                child: ListView(
+                child: ListView.builder(
+                  controller: _scrollController,
                   padding: const EdgeInsets.symmetric(vertical: 10),
-                  children: const [
-                    MessageCard(
-                      message: "Your Typed message is here ",
-                      isSender: true,
-                    ),
-                    MessageCard(
-                      message: "Received message goes here",
-                      isSender: false,
-                    ),
-                    // Add more message cards as needed
-                  ],
+                  itemCount: _messages.length,
+                  itemBuilder: (context, index) {
+                    return _messages[index];
+                  },
                 ),
               ),
             ),
+            _buildMessageInput(),
           ],
         ),
       ),
     );
   }
+
+  Widget _buildMessageInput() {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Row(
+        children: [
+          Expanded(
+            child: TextField(
+              controller: _messageController,
+              decoration: const InputDecoration(
+                hintText: 'Type a message...',
+              ),
+            ),
+          ),
+          IconButton(
+            onPressed: _sendMessage,
+            icon: const Icon(Icons.send),
+          ),
+        ],
+      ),
+    );
+  }
 }
-
-
-
