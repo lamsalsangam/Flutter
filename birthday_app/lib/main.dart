23390e0016a689eb4f4fd283d8edc1a0dc2d65cd
lamsalsangam <me.sangam.lamsal.10@gmail.com>
2023-09-18 15:20:24 +0545

    Refactored the existing code

--- birthday_app/lib/main.dart
+++ birthday_app/lib/main.dart
@@ -13,47 +13,45 @@ class MyApp extends StatelessWidget {
       title: "Your Day",
       home: Scaffold(
         body: SafeArea(
-          child: Column(
-            children: [
-              Row(
-                children: [
-                  Expanded(
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Container(
-                        decoration: const BoxDecoration(
-                          color: Colors.deepPurple,
-                          borderRadius: BorderRadius.all(Radius.circular(10)),
-                        ),
-                        height: 200,
-                        alignment: Alignment.center,
-                        child: const Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            CircleAvatar(
-                              backgroundColor: Colors.transparent,
-                              radius: 40,
-                              child: Icon(
-                                Icons.person_2_sharp,
-                                size: 50,
-                              ),
-                            ),
-                            Text(
-                              "Proteus",
-                              style: TextStyle(
-                                  fontSize: 25,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.white),
-                            )
-                          ],
-                        ),
-                      ),
-                    ),
-                  )
-                ],
-              )
-            ],
+          child: Center(
+            child: _buildProfileCard(),
           ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildProfileCard() {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.deepPurple,
+          borderRadius: BorderRadius.circular(10),
+        ),
+        height: 200,
+        alignment: Alignment.center,
+        child: const Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CircleAvatar(
+              backgroundColor: Colors.transparent,
+              radius: 40,
+              child: Icon(
+                Icons.person_2_sharp,
+                size: 50,
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              "Proteus",
+              style: TextStyle(
+                fontSize: 25,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+          ],
         ),
       ),
     );
