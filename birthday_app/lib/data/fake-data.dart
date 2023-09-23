3371b855d64338ffe353888983e0ada7c64ebf52
lamsalsangam <me.sangam.lamsal.10@gmail.com>
2023-09-23 18:42:18 +0545

    Just added the fake data

---
+++ birthday_app/lib/data/fake-data.dart
@@ -0,0 +1,18 @@
+final fakeData = [
+  {"name": "John Doe", "dateOfBirth": "1990-05-15"},
+  {"name": "Jane Smith", "dateOfBirth": "1985-12-08"},
+  {"name": "David Johnson", "dateOfBirth": "1992-07-25"},
+  {"name": "Emily Davis", "dateOfBirth": "1988-03-10"},
+  {"name": "Michael Wilson", "dateOfBirth": "1995-09-02"},
+  {"name": "Sarah Brown", "dateOfBirth": "1984-11-18"},
+  {"name": "James Lee", "dateOfBirth": "1993-01-31"},
+  {"name": "Laura Taylor", "dateOfBirth": "1991-06-20"},
+  {"name": "Daniel Clark", "dateOfBirth": "1987-04-14"},
+  {"name": "Olivia Martin", "dateOfBirth": "1994-08-09"},
+  {"name": "William Anderson", "dateOfBirth": "1986-10-27"},
+  {"name": "Ava White", "dateOfBirth": "1996-02-04"},
+  {"name": "Robert Hall", "dateOfBirth": "1989-09-14"},
+  {"name": "Sophia Garcia", "dateOfBirth": "1997-12-01"},
+  {"name": "Ethan Perez", "dateOfBirth": "1983-07-07"}
+];
+
