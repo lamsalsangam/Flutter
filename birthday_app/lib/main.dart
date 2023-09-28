0c8b0c01c3fa9dc69cffa06d3ee92e357ccf467b
lamsalsangam <me.sangam.lamsal.10@gmail.com>
2023-09-28 19:47:50 +0545

    Displayed the section for the remaining birthday also

--- birthday_app/lib/main.dart
+++ birthday_app/lib/main.dart
@@ -28,18 +28,17 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     final today = DateTime.now();
 
-    final upcomingBirthdays = fakeData
-        .where((person) {
+    final upcomingBirthdays = fakeData.where((person) {
       final birthday = DateTime.parse(person["dateOfBirth"]);
       final daysUntilBirthday = birthday.isBefore(today)
           ? birthday.add(const Duration(days: 365)).difference(today).inDays
           : birthday.difference(today).inDays;
       return daysUntilBirthday <= 90; // Consider birthdays within 90 days
-    })
-        .toList();
+    }).toList();
 
     final closestBirthdays = upcomingBirthdays.isNotEmpty
-        ? upcomingBirthdays.sublist(0, upcomingBirthdays.length >= 3 ? 3 : upcomingBirthdays.length)
+        ? upcomingBirthdays.sublist(
+            0, upcomingBirthdays.length >= 3 ? 3 : upcomingBirthdays.length)
         : [];
 
     final todayBirthdays = fakeData.where((person) {
@@ -49,7 +48,12 @@ class MyApp extends StatelessWidget {
 
     final cards = todayBirthdays.isNotEmpty
         ? todayBirthdays
-        : closestBirthdays.isNotEmpty ? closestBirthdays : [];
+        : closestBirthdays.isNotEmpty
+            ? closestBirthdays
+            : [];
+
+    final remainingBirthdays =
+        upcomingBirthdays.where((person) => !cards.contains(person)).toList();
 
     return MaterialApp(
       title: "Your Day",
@@ -74,13 +78,15 @@ class MyApp extends StatelessWidget {
                 child: Row(
                   children: cards
                       .map((card) => Padding(
-                    padding: const EdgeInsets.all(8.0), // Add padding between cards
-                    child: _buildProfileCard(card),
-                  ))
+                            padding: const EdgeInsets.all(8.0),
+                            child: _buildProfileCard(card),
+                          ))
                       .cast<Widget>()
                       .toList(),
                 ),
               ),
+              const SizedBox(height: 10),
+              _buildRemainingBirthdays(remainingBirthdays),
             ],
           ),
         ),
@@ -108,6 +114,82 @@ class MyApp extends StatelessWidget {
             child: Icon(
               Icons.person,
               size: 50,
+            ),
+          ),
+          const SizedBox(height: 10),
+          Text(
+            name ?? 'Unknown',
+            style: const TextStyle(
+              fontSize: 25,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildRemainingBirthdays(
+      List<Map<String, dynamic>> remainingBirthdays) {
+    return Expanded(
+      child: Container(
+          color: Colors.grey[200],
+          // Background color for remaining birthdays section
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                "Birthdays",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.blue, // Customize the text color
+                ),
+              ),
+              const SizedBox(height: 10),
+              Expanded(
+                child: ListView.builder(
+                  itemCount: remainingBirthdays.length,
+                  itemBuilder: (context, index) {
+                    final person = remainingBirthdays[index];
+                    final name = person['name']?.toString();
+                    return Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: ListTile(
+                          leading: const Icon(Icons.person),
+                          title: Text(name ?? '')),
+                    );
+                  },
+                ),
+              ),
+            ],
+          )),
+    );
+  }
+
+  Widget _buildRemainingBirthdayCard(Map<String, dynamic> person) {
+    final name = person['name']?.toString();
+
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.lightBlue, // Customize the background color
+        borderRadius: BorderRadius.circular(10),
+      ),
+      width: 380,
+      height: 150,
+      alignment: Alignment.center,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const CircleAvatar(
+            backgroundColor: Colors.transparent,
+            radius: 40,
+            child: Icon(
+              Icons.person,
+              size: 50,
+              color: Colors.white, // Customize the icon color
             ),
           ),
           const SizedBox(height: 10),
